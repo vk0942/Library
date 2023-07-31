605028cd68e7747162309c458bd051bc9156ef80
vk0942 <vanshkathnawal0942@gmail.com>
2023-07-31 16:25:28 +0530

    added form validity

--- script.js
+++ script.js
@@ -2,26 +2,30 @@ let my_library = [];
 const shelf = document.querySelector('.shelf');
 const submit_book = document.querySelector('#submit-book');
 const form = document.querySelector('.popup-content');
-
-function create_addBook(title,author,pages,read)
-{
+const Title_input = document.querySelector('#Title');
+const Author_input = document.querySelector('#Author');
+const Pages_input = document.querySelector('#Pages');
+const Title_error = document.querySelector('.Title_error');
+const Author_error = document.querySelector('.Author_error');
+const Pages_error = document.querySelector('.Pages_error');
+console.log(Title_input);
+console.log(Title_error);
+function create_addBook(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     my_library.push(book);
-    const block =  createBlock(book);
+    const block = createBlock(book);
     shelf.appendChild(block);
-    
+
 }
 //constructor
-function Book(title,author,pages,read)
-{
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
 }
-function createBlock(book)
-{
+function createBlock(book) {
     const i = my_library.indexOf(book);
     const B = document.createElement('div');
     const one = document.createElement('div');
@@ -31,33 +35,33 @@ function createBlock(book)
     const five = document.createElement('div');
     const toggle = document.createElement('input');
     const label = document.createElement('label');
-    
+
     const Delete = document.createElement('button');
     Delete.textContent = "DELETE BOOK";
     Delete.classList.add("Del-btn");
-    Delete.id = 'd_'+i.toString();
-    Delete.addEventListener('click',()=>{
+    Delete.id = 'd_' + i.toString();
+    Delete.addEventListener('click', () => {
         let index = Delete.id.substring(2);
-        let b_id = 'b_'+index;
+        let b_id = 'b_' + index;
         shelf.removeChild(document.getElementById(b_id));
-        my_library.splice(parseInt(index),1);
+        my_library.splice(parseInt(index), 1);
         console.log(my_library);
     })
     label.textContent = "Read";
     toggle.type = 'checkbox';
-    toggle.id = 'toggle_'+i.toString();
-    label.setAttribute('for','toggle_'+i.toString());
+    toggle.id = 'toggle_' + i.toString();
+    label.setAttribute('for', 'toggle_' + i.toString());
     toggle.checked = book.read;
     B.classList.add('block');
-    B.id = 'b_'+i.toString();
-    
+    B.id = 'b_' + i.toString();
+
     one.textContent = book.title;
-    two.textContent = 'By: '+ book.author;
+    two.textContent = 'By: ' + book.author;
     three.textContent = `Pages: ${book.pages}`;
     four.appendChild(label);
     four.appendChild(toggle);
     five.appendChild(Delete);
-    
+
     B.appendChild(one);
     B.appendChild(two);
     B.appendChild(three);
@@ -65,53 +69,90 @@ function createBlock(book)
     B.appendChild(five);
     return B;
 }
-Book.prototype.info = function(){
-   
+Book.prototype.info = function () {
+
     return `The ${this.title} by ${this.author} , ${this.pages} pages read = ${this.read}`;
 }
 
-create_addBook("karma Yog" , "Swami Vivekananda" , 300 , true);
-create_addBook("Bhakti Yog" , "Swami Vivekananda" , 100 , true);
-create_addBook("Raja Yog" , "Swami Vivekananda" , 400 , false);
-create_addBook("Gyaan Yog" , "Swami Vivekananda" , 300 , false);
+create_addBook("karma Yog", "Swami Vivekananda", 300, true);
+create_addBook("Bhakti Yog", "Swami Vivekananda", 100, true);
+create_addBook("Raja Yog", "Swami Vivekananda", 400, false);
+create_addBook("Gyaan Yog", "Swami Vivekananda", 300, false);
 
 
-// my_library.forEach((book)=>{
-   
-//     const B = createBlock(book);
-//     shelf.appendChild(B);
-// });
 
-document.querySelector('#new-book').addEventListener('click',()=>{
+
+document.querySelector('#new-book').addEventListener('click', () => {
     document.querySelector('.popup').style.display = "flex";
 })
-document.querySelector('#cancel').addEventListener('click',()=>{
+document.querySelector('#cancel').addEventListener('click', () => {
     document.querySelector('.popup').style.display = "none";
     form.reset();
 })
-form.addEventListener("change", () => {
-    submit_book.disabled = !form.checkValidity()
-});
-form.addEventListener('submit',(event) =>{
+// form.addEventListener("change", () => {
+//     submit_book.disabled = !form.checkValidity()
+// });
+
+form.addEventListener('submit', (event) => {
+    if (!Title_input.validity.valid) {
+
+        showError(Title_input, Title_error);
+        event.preventDefault();
+    } else if (!Author_input.validity.valid) {
+
+        showError(Author_input, Author_error);
+        event.preventDefault();
+    } else if (!Pages_input.validity.valid) {
+
+        showError(Pages_input, Pages_error);
+        event.preventDefault();
+    }else{
     event.preventDefault();
     const titleValue = document.getElementById('Title').value;
     const authorValue = document.getElementById('Author').value;
     const pagesValue = document.getElementById('Pages').value;
     const readValue = document.getElementById('Read').checked;
-    
-    create_addBook(titleValue,authorValue,pagesValue,readValue);
+
+    create_addBook(titleValue, authorValue, pagesValue, readValue);
     document.querySelector('.popup').style.display = "none";
     form.reset();
+    }
 })
 
+function showError(input_field, error_field) {
+    if (input_field.validity.valueMissing) {
+        error_field.textContent = "You need to Fill this field ";
+    } else if (input_field.validity.typeMismatch) {
+        error_field.textContent = "Entered value needs to be an email address.";
+    }
 
-// for (const element of Del_btns) {
-//     element.addEventListener('click', () => {
-//        let elm_id = element.id;
-//        let b_id = 'b'+elm_id.substring(1);
-//        let index = parseInt(b_id.toString(2));
-//        shelf.removeChild(document.getElementById(b_id));
-//        my_library.splice(index,1);
-//        console.log(my_library);
-//     });
-//   }
+    error_field.className = "error_active";
+}
+
+Title_input.addEventListener("input", (event) => {
+
+    if (Title_input.validity.valid) {
+        Title_error.textContent = "";
+        Title_error.className = "error";
+    } else {
+        showError(Title_input, Title_error);
+    }
+});
+Pages_input.addEventListener("input", (event) => {
+
+    if (Pages_input.validity.valid) {
+        Pages_error.textContent = "";
+        Pages_error.className = "error";
+    } else {
+        showError(Pages_input, Pages_error);
+    }
+});
+Author_input.addEventListener("input", (event) => {
+
+    if (Author_input.validity.valid) {
+        Author_error.textContent = "";
+        Author_error.className = "error";
+    } else {
+        showError(Author_input, Author_error);
+    }
+});
